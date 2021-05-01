c38046efefa74edba3d375510ad4c0812eb572c7
yhu4592 <hu.yongkangsg114@gmail.com>
2021-05-01 00:06:22 -0500

    Fixed some rubocop errors and edited comments.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,13 +6,14 @@ class TicTacToe
     @board = [['1', '2', '3'],
               ['4', '5', '6'],
               ['7', '8', '9']]
-    print "Player 1, enter your marker: "
-    @mark_1 = gets.chomp
-    print "Player 2, enter your marker: "
-    @mark_2 = gets.chomp
+    print 'Player 1, enter your marker: '
+    @mark1 = gets.chomp
+    print 'Player 2, enter your marker: '
+    @mark2 = gets.chomp
     play_game
   end
 
+  # Displays board to stdout
   def display_board
     puts '  -   -   -'
     @board.each do |row|
@@ -22,46 +23,43 @@ class TicTacToe
       print "|\n  -   -   -\n"
     end
   end
-    #loop until checks return winner, loop max 9 times
-    #calls player turns
-    #call checks
-    #reset if there is a winner
+
+  # Executes game logic
   def play_game
     display_board
     winner = false
     4.times do
-      print "Player 1, enter the coordinate for your mark: "
+      print 'Player 1, enter the coordinate for your mark: '
       coordinate = gets.chomp.to_i
       place_marker(@mark_1, coordinate)
       display_board
-      if check_column || check_row || check_diagonal 
+      if check_column || check_row || check_diagonal
         winner = true
-        puts "Winner is Player 1."
+        puts 'Winner is Player 1.'
         break
       end
-      print "Player 2, enter the coordinate for your mark: "
+      print 'Player 2, enter the coordinate for your mark: '
       coordinate = gets.chomp.to_i
       place_marker(@mark_2, coordinate)
       display_board
-      if check_column || check_row || check_diagonal 
+      if check_column || check_row || check_diagonal
         winner = true
-        puts "Winner is Player 2."
+        puts 'Winner is Player 2.'
         break
       end
     end
-    print "Player 1, enter the coordinate for your mark: "
+    print 'Player 1, enter the coordinate for your mark: '
     coordinate = gets.chomp.to_i
     place_marker(@mark_1, coordinate)
     display_board
     if check_column || check_row || check_diagonal 
       winner = true
-      puts "Winner is Player 1."
+      puts 'Winner is Player 1.'
     end
-    if !winner
-      puts "Game is tied."
-    end
+    puts 'Game is tied.' unless winner
   end
 
+  # Places player marker on board on coordinate
   def place_marker(marker, coordinate)
     if coordinate.between?(1, 3)
       @board[0][coordinate - 1] = marker
@@ -72,45 +70,42 @@ class TicTacToe
     end
   end
 
-  # Conditions
+  # Checks for winning row combinations
   def check_row
-    if @board[0][0].eql?(@board[0][1]) and @board[0][0].eql?(@board[0][2])
-      return true
-    elsif @board[1][0].eql?(@board[1][1]) and @board[1][0].eql?(@board[1][2])
-      return true
-    elsif @board[2][0].eql?(@board[2][1]) and @board[2][0].eql?(@board[2][2])
-      return true
-    end
-    return false
+    return true if @board[0][0].eql?(@board[0][1]) && @board[0][0].eql?(@board[0][2])
+
+    return true if @board[1][0].eql?(@board[1][1]) && @board[1][0].eql?(@board[1][2])
+
+    return true if @board[2][0].eql?(@board[2][1]) && @board[2][0].eql?(@board[2][2])
+
+    false
   end
 
+  # Checks for winning column combinations
   def check_column
-    #loop down each column, checks for winner
-    if @board[0][0].eql?(@board[1][0]) and @board[0][0].eql?(@board[2][0])
-      return true
-    elsif @board[0][1].eql?(@board[1][1]) and @board[0][1].eql?(@board[2][1])
-      return true
-    elsif @board[0][2].eql?(@board[1][2]) and @board[0][2].eql?(@board[2][2])
-      return true
-    end
-    return false
+    return true if @board[0][0].eql?(@board[1][0]) && @board[0][0].eql?(@board[2][0])
+
+    return true if @board[0][1].eql?(@board[1][1]) && @board[0][1].eql?(@board[2][1])
+
+    return true if @board[0][2].eql?(@board[1][2]) && @board[0][2].eql?(@board[2][2])
+
+    false
   end
 
+  # Checks for winning diagonal combinations
   def check_diagonal
-    if @board[0][0].eql?(@board[1][1]) and @board[0][0].eql?(@board[2][2])
-      return true
-    elsif @board[0][2].eql?(@board[1][1]) and @board[0][2].eql?(@board[2][0])
-      return true
-    end  
-    return false
+    return true if @board[0][0].eql?(@board[1][1]) && @board[0][0].eql?(@board[2][2])
+
+    return true if @board[0][2].eql?(@board[1][1]) && @board[0][2].eql?(@board[2][0])
+
+    false
   end
 
   def reset
-    for i in (1..9) do
-      place_marker(i.to_s, i)
+    9.times do |i|
+      place_marker((i + 1).to_s, i)
     end
   end
 end
-
 
 board = TicTacToe.new
